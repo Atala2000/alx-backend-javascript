ea050d50c805d221d8be0263976b52a11392b8c3
Atala2000 <atalakidi@gmail.com>
2024-03-07 01:04:28 +0300

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoData, userData]) => {
-      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+async function handleProfileSignup() {
+    try {
+        const [photoData, userData] = await Promise.all([uploadPhoto(), createUser()]);
+        console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    } catch (error) {
+        console.log('Signup system offline');
+    }
 }
