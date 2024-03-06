4a0a6749a9a0503640823a5c04b7f8f0de9fe804
Atala2000 <atalakidi@gmail.com>
2024-03-07 01:05:48 +0300

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-async function handleProfileSignup() {
+export async function handleProfileSignup() {
     try {
         const [photoData, userData] = await Promise.all([uploadPhoto(), createUser()]);
         console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
