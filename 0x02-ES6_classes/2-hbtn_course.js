10a3997922b2c90a9d4e6d4712d3ce62ae616a16
Atala2000 <atalakidi@gmail.com>
2024-03-07 22:40:01 +0300

    Task 1 to 3

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a string');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      this._students = students;
+    }
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  set length(value) {
+    this._length = value;
+  }
+
+  set students(value) {
+    this._students = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string') {
+      throw new TypeError('Value must be string');
+    } else {
+      this._code = code;
+      this._name = name;
+    }
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
