dc24ff8ad3decd2f609f944295fbdf5296c94057
Atala2000 <atalakidi@gmail.com>
2024-03-08 21:48:04 +0300

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,18 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise.then(
-      () => {
-        resolve({
-          status: 200,
-          body: 'success',
-        });
-        console.log('Got a response from the API');
-      },
-    ).catch(
-      () => {
-        reject(new Error());
-        console.log('Got a response from the API');
-      },
-    );
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
